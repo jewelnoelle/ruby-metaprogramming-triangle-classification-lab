e56d3b07f36e8b888285a4d1ea830fe0838f02a3
Jewel Carrier-Davis <jujicb@gmail.com>
2021-03-23 16:54:33 -0400

    commit tree 15754e819d96b4986bfc9df663776ec9f84f300d

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,46 @@
 class Triangle
-  # write code here
+  attr_accessor :side_1, :side_2, :side_3, :all_sides
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+    @all_sides = [@side_1, @side_2, @side_3].sort
+  end
+
+  def kind
+    if real_triangle
+      begin
+        raise TriangleError
+      end
+    end
+
+    if side_1 == side_2 && side_2 == side_3
+      return :equilateral
+    elsif side_1 == side_2 || side_1 == side_3 || side_2 == side_3
+      return :isosceles
+    else
+      return :scalene
+    end
+    
+    # if side_1 + side_2 < side_3 || side_2 + side_3 < side_1 || side_1 + side_3 < side_2
+    #     raise TriangleError
+    # elsif side_1 = 0 || side_2 = 0 || side_3 = 0
+    #     raise TriangleError
+    # elsif side_1 < 0 || side_2 < 0 || side_3 < 0
+    #     raise TriangleError
+    # end
+  end
+
+  def real_triangle
+    self.all_sides.any? do | side |
+    all_sides.any? do | side | 
+      side < 0 || all_sides[0] + all_sides[1] <= all_sides[2] 
+    end
+  end
+end
+
+  class TriangleError < StandardError
+  end
+
 end
